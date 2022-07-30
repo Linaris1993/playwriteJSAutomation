751687525aa145522a73a458f76a6680ae876a1f
Linaris1993 <linakhatmullina@gmail.com>
2022-07-30 19:32:55 -0400

    added another test

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Browser Context PLaywrite test', async ({browser}) => {
-const context = await browser.newContext();
+test('Browser Context PLaywrite test', async ({browser}) => {
+
 const page = await context.newPage();
 const userName = page.locator('#username');
 const signIn = page.locator("#signInBtn");
@@ -21,7 +21,7 @@ signIn.click(),
 );
 
 console.log(await page.locator("[style*='block']").textContent());
-await expect().toContainText('Incorrect');
+await expect(page.locator("[style*='block']")).toContainText('Incorrect');
 
 //type - fill
 await userName.fill(""); //clear the field
@@ -33,9 +33,41 @@ const allTitles = await cardTitles.allTextContents();
 console.log(allTitles);
 });
 
-test('Page PLaywrite test', async ({page}) => {
- 
-    await page.goto("http://google.com");
-    await page.title();
-    await expect(page).toHaveTitle("Google")
+test('UI Controls', async ({page}) => {
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const dropdown = page.locator("select.form-control");
+    const documentLink = page.locator("[href*='documents-request']")
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last();
+    await page.locator("okayBtn").click();
+  //  await page.pause();
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck(); 
+    expect(await page.locator("#terms").isChecked()).toBeFalsy(); //assert el to be Unchecked
+    await expect(documentLink).toHaveAttribute("class", "blinkingText");
 });
+
+test('Child Windows Handling', async ({browser}) => {
+    const context = await browser.newContext(); //new session = window
+    const page = await context.newPage(); //new page
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+    const documentLink = page.locator("[href*='documents-request']");
+    const userName = page.locator('#username');
+
+    const [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    documentLink.click(),
+    ])
+
+   text = await newPage.locator(".red").textContent();
+   const arrayText = text.split("@")
+   const domain = arrayText[1].split(" ")[0];
+   console.log(domain);
+   await page.locator("#userName").type(domain);
+   await page.pause();
+});
+
